d6eb744e780301ecc1906ab403cacfdf205d9c47
AMITA YADAV <yadavamitaaim@gmail.com>
2026-06-14 17:26:32 +0530

    Add files via upload

---
+++ function .py
@@ -0,0 +1,49 @@
+movie = [ ]
+movie1 = input ("enter 1 movie: ")
+movie2 = input ("enter 2 movie: ")
+movie3 = input ("enter 3 movie: ")
+
+movie . append (movie1)
+
+movie . append (movie2)
+
+movie . append (movie3)
+
+print(movie)
+
+   
+
+
+
+
+  
+   
+    
+   
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
